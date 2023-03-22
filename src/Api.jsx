bfac24b76117bff5b840cf726f2a089361797765
Jobayer <jobayerrahman0220@gmail.com>
2023-03-22 18:12:45 +0600

    connect api

---
+++ src/Api.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const searchImages = async () => {
+  const url = "https://api.unsplash.com/search/photos";
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: "Client-ID VJJ1XX_9_BbF7S7fsz9HJ7lpjDUltmtQnTBW1ehh2dg",
+    },
+    params: {
+      query: "car",
+    },
+  });
+
+  //   console.log(response);
+};
+
+export default searchImages;
